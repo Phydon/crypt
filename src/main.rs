60505479840d6f90d3196f23a3bc631cb758f2da
Phydon <leann.phydon@gmail.com>
2022-06-18 18:27:45 +0200

    first stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+const LST: [char; 13] = [
+        'a', 
+        'b', 
+        'c', 
+        'd', 
+        'e', 
+        'f', 
+        'g', 
+        'h', 
+        'i', 
+        'j', 
+        'k', 
+        'l', 
+        'm'
+    ];
+
+#[derive(Debug)]
+enum Key {
+    K1(u8),
+    K2(u8),
+}
+
+#[derive(Debug)]
+struct ThreeCrypt {
+    key: Key,
+    lst: [char; 13],
+}
+
 fn main() {
-    println!("Hello, world!");
+    let code = ThreeCrypt {
+        key: Key::K1(3),
+        lst: LST,
+    };
+
+    println!("code: {:?}", code);
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_test() {
+        let indeed: bool = true;
+        assert!(indeed);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_test() {
+        panic!();
+    }
 }
